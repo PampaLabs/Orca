d1ae5ecbf1f2b5dc01b3e8edebaaa3d46b997c34
Facundo Lespi <facu.lespi@gmail.com>
2025-02-03 21:28:32 -0300

    Setup readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,72 +1,35 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
 
-# -- Path setup --------------------------------------------------------------
+# -- Project information
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+project = 'Orca'
+copyright = '2025, Orca'
+author = 'Orca'
 
-# for Sphinx-1.4 or newer
-extensions = ['recommonmark']
+release = '0.1'
+version = '0.1.0'
 
-# for Sphinx-1.3
-from recommonmark.parser import CommonMarkParser
+# -- General configuration
 
-source_parsers = {
-    '.md': CommonMarkParser,
-}
-
-source_suffix = ['.rst', '.md']
-
-# -- Project information -----------------------------------------------------
-
-project = 'Balea'
-copyright = '2019, Xabaril'
-author = 'Xabaril'
-
-# The full version, including alpha/beta/rc tags
-release = '1.0.0'
-
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
 ]
 
-# Add any paths that contain templates here, relative to this directory.
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+# -- Options for HTML output
 
-# The master toctree document.
-master_doc = 'index'
+html_theme = 'sphinx_rtd_theme'
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "sphinx_rtd_theme"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'default'
-highlight_language = 'csharp'
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
