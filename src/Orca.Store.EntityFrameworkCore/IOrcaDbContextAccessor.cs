cfa887be63ec4c8e5a93dfd6e0764a24612aed7e
Facundo Lespi <facu.lespi@gmail.com>
2025-07-05 18:48:47 -0300

    Remove warnings

--- src/Orca.Store.EntityFrameworkCore/IOrcaDbContextAccessor.cs
+++ src/Orca.Store.EntityFrameworkCore/IOrcaDbContextAccessor.cs
@@ -2,10 +2,17 @@
 
 namespace Orca.Store.EntityFrameworkCore;
 
+/// <summary>
+/// Provides access to the <see cref="Microsoft.EntityFrameworkCore.DbContext"/>.
+/// </summary>
 public interface IOrcaDbContextAccessor
 {
+    /// <summary>
+    /// Gets the <see cref="Microsoft.EntityFrameworkCore.DbContext"/>
+    /// </summary>
     DbContext DbContext { get; }
 }
+
 
 internal class OrcaDbContextAccessor : IOrcaDbContextAccessor
 {
